d72f16247a6cff660c1b07938d0b55846f31c01d
EminDege <emin.dege@gmail.com>
2023-08-29 22:51:29 +0300

    temel oluşturuldu

--- src/App.js
+++ src/App.js
@@ -4,17 +4,55 @@ import DrawApp from './drawApp';
 
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [turValue, setTurValue] = useState('klasik');
+  const [drawResult, setDrawResult] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleTurChange = (event) => {
+    setTurValue(event.target.value)
+  };
+  const drawClick = () => {
+    const result = <DrawApp tur={turValue} uye={inputValue} />;
+    setDrawResult(result);
+  };
+
   return (
-    <div>
-      <DrawApp />
+    <div className="draw-app">
+      <div className="input-container">
+        <textarea
+          rows="10"
+          placeholder="Katılımcıları boşluklarla ayırarak girin"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="detaylar">
+
+        <select name='tur' onChange={handleTurChange}>
+          <option value={"klasik"}>Klasik Çekiliş</option>
+          <option value={"ylbası"}>Yılbaşı Çekilişi</option>
+          <option value={"eslestirme"}>Eşleştirme Çekilişi</option>
+          <option value={"gruplama"}>Gruplama Çekilişi</option>
+        </select> <br />
+
+
+      </div>
+      <div className=''>
+        <button onClick={drawClick}>Çekilişi Yap</button>
+      </div>
+      <div className='Sonuc'>
+
+        <h2>Sonuç</h2>
+        {drawResult}
+      </div>
 
     </div>
-
-
-
-
-
   )
 }
+
+
 
 export default App;
--- src/drawApp.js
+++ src/drawApp.js
@@ -1,55 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import "./drawApp.css";
 
-const DrawApp = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [participants, setParticipants] = useState([]);
+function DrawApp(props) {
+    const tur = props.tur;
+    const uye = props.uye.split("\n");
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+    let winner = '';
 
-    const handleAddParticipant = () => {
-        if (inputValue.trim() !== '') {
-            setParticipants((prevParticipants) => [...prevParticipants, inputValue]);
-            setInputValue('');
-        }
-    };
-
+    if (tur === "klasik") {
+        const index = Math.floor(Math.random() * (uye.length))
+        winner = uye[index];
+    }
     return (
-        <div className="draw-app">
-            <div className="input-container">
-                <textarea
-                    rows="10"
-                    placeholder="Katılımcıları boşluklarla ayırarak girin"
-                    value={inputValue}
-                    onChange={handleInputChange}
-                />
-                <button className='listAddButton' onClick={handleAddParticipant}>Katılımcı Ekle</button>
-            </div>
-
-            <div className="detaylar">
-                <h3>Katılımcılar: {participants}</h3>
-                <select>
-                    <option value={"klasik"}>Klasik Çekiliş</option>
-                    <option value={"ylbası"}>Yılbaşı Çekilişi</option>
-                    <option value={"eslestirme"}>Eşleştirme Çekilişi</option>
-                    <option value={"gruplama"}>Gruplama Çekilişi</option>
-                </select> <br />
-
-            </div>
-            <div className=''>
-                <button>Çekilişi Yap</button>
-            </div>
-            <div className='Sonuc'>
-                <h2>SONUÇ</h2>
-
-
-            </div>
-        </div>
-    );
-};
-
-export default DrawApp;
+        <div>{winner}</ div>
+    )
+}
+export default DrawApp
 
 
