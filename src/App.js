15ffb68d8b6eeeb40fa2a16a54c41f4c177c6d14
EminDege <emin.dege@gmail.com>
2023-08-31 12:52:19 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import DrawApp from './drawApp';
 
@@ -7,6 +7,9 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [turValue, setTurValue] = useState('klasik');
   const [drawResult, setDrawResult] = useState(null);
+  const [kazananSayisi, setKazananSayisi] = useState(1);
+  const [bges, setBges] = useState(2);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -14,10 +17,24 @@ function App() {
   const handleTurChange = (event) => {
     setTurValue(event.target.value)
   };
+  const handleKazananSayisi = (event) => {
+    setKazananSayisi(event.target.value)
+  };
+  const handleBges = (event) => {
+    setBges(event.target.value)
+  };
   const drawClick = () => {
     const result = <DrawApp tur={turValue} uye={inputValue} />;
     setDrawResult(result);
   };
+
+  const handleChangeVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    handleChangeVisibility()
+  }, [turValue])
 
   return (
     <div className="draw-app">
@@ -29,22 +46,27 @@ function App() {
           onChange={handleInputChange}
         />
       </div>
-      <div className="detaylar">
+      <div className="">
+        <h2>Çekiliş Türü</h2>
 
         <select name='tur' onChange={handleTurChange}>
           <option value={"klasik"}>Klasik Çekiliş</option>
           <option value={"yilbasi"}>Yılbaşı Çekilişi</option>
           <option value={"gruplama"}>Gruplama Çekilişi</option>
-        </select> <br />
-
+        </select>
 
       </div>
       <div className=''>
-        <button onClick={drawClick}>Çekilişi Yap</button>
+        <h2>Seçenekler</h2>
+        <div id='kazananblok'><h4>Kazanan Sayısı</h4>
+          <input type='number' value={kazananSayisi} onChange={handleKazananSayisi}></input></div>
+        <div id='bgesblok'><h4>Bir gruptaki eleman sayısı</h4>
+          <input type='number' value={bges} onChange={handleBges}></input></div>
       </div>
-      <div className='Sonuc'>
 
-        <h2>Sonuç</h2>
+      <div className='Sonuc'>
+        <button onClick={drawClick}>Çekilişi Yap</button>
+        <h1>Sonuç</h1>
         {drawResult}
       </div>
 
