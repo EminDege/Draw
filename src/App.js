1ea87875ad5e862fa602ebe73b12f5d03e775f1c
EminDege <emin.dege@gmail.com>
2023-09-15 16:52:10 +0300

    responsive düzenlemeleri yapıldı

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import DrawApp from './drawApp';
 
-
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [inputValue2, setInputValue2] = useState('');
@@ -13,6 +12,14 @@ function App() {
   const [isVisibleWinner, setIsVisibleWinner] = useState(true);
   const [isVisibleBges, setIsVisibleBges] = useState(false);
   const [isVisibleInput, setIsVisibleInput] = useState(false);
+  const [isVisibleMatch, setIsVisibleMatch] = useState(false);
+  const [isVisibleDontMatch, setIsVisibleDontMatch] = useState(false);
+  const [matchOptions, setMatchOptions] = useState([]);
+  const [pairMatchOptions, setPairMatchOptions] = useState([]);
+  const [matchValue1, setMatchValue1] = useState('');
+  const [matchValue2, setMatchValue2] = useState('');
+  const [dontMatchValue1, setDontMatchValue1] = useState('');
+  const [dontMatchValue2, setDontMatchValue2] = useState('');
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -29,9 +36,11 @@ function App() {
   const handleBges = (event) => {
     setBges(event.target.value)
   };
+
   const drawClick = () => {
     const result = <DrawApp type={typeValue} member={inputValue} member2={inputValue2}
-      groupSize={bges} numberOfWinner={numberOfWinner} />;
+      groupSize={bges} numberOfWinner={numberOfWinner} matchValue1={matchValue1}
+      matchValue2={matchValue2} dontMatchValue1={dontMatchValue1} dontMatchValue2={dontMatchValue2} />;
     setDrawResult(result);
   };
 
@@ -39,26 +48,38 @@ function App() {
     setDrawResult("");
   }, [typeValue])
 
+  useEffect(() => {
+    setMatchOptions(inputValue.split("\n").filter(item => item !== null && item !== undefined && item !== ""))
+    setPairMatchOptions(inputValue2.split("\n").filter(item => item !== null && item !== undefined && item !== ""))
+  }, [inputValue, inputValue2])
 
   useEffect(() => {
     if (typeValue === 'classic') {
       setIsVisibleWinner(true);
       setIsVisibleBges(false);
       setIsVisibleInput(false);
+      setIsVisibleMatch(false);
+      setIsVisibleDontMatch(false);
     } else if (typeValue === 'grouping') {
       setIsVisibleWinner(false);
       setIsVisibleBges(true);
       setIsVisibleInput(false);
+      setIsVisibleMatch(true);
+      setIsVisibleDontMatch(true);
     }
     else if (typeValue === 'pairing') {
       setIsVisibleWinner(false);
       setIsVisibleBges(false);
       setIsVisibleInput(true);
+      setIsVisibleMatch(true);
+      setIsVisibleDontMatch(true);
     }
     else {
       setIsVisibleWinner(false);
       setIsVisibleBges(false);
       setIsVisibleInput(false);
+      setIsVisibleMatch(true);
+      setIsVisibleDontMatch(true);
     }
   }, [typeValue]);
 
@@ -74,6 +95,7 @@ function App() {
           <option value={"pairing"}>Pairing Draw</option>
         </select>
       </div>
+
       <div className="inputContainer">
         <textarea
           rows="10"
@@ -88,6 +110,7 @@ function App() {
           onChange={handleInputChange2}
         />
       </div>
+
       <div className='optionPlusButton'>
         <div className='options'>
           <h3>Options</h3>
@@ -95,14 +118,55 @@ function App() {
             <input type='number' value={numberOfWinner} onChange={handleNumberOfWinner}></input></div>
           <div className='option' style={{ display: isVisibleBges ? 'flex' : 'none' }}><h4>Group Size :</h4>
             <input type='number' value={bges} onChange={handleBges}></input></div>
+          <div className='option' style={{ display: isVisibleMatch ? 'flex' : 'none' }}>
+            <h4 className='match'>Match :</h4>
+            <select value={matchValue1} onChange={(event) => setMatchValue1(event.target.value)}>
+              <option value="">Choose</option>
+              {matchOptions.map((option, index) => (
+                <option value={option} key={index}>{option}</option>
+              ))}
+            </select> &
+            <select value={matchValue2} onChange={(event) => setMatchValue2(event.target.value)}>
+              <option value="">Choose</option>
+              {typeValue === 'pairing' ? (
+                pairMatchOptions.map((option, index) => (
+                  <option value={option} key={index}>{option}</option>
+                ))
+              ) : (
+                matchOptions.map((option, index) => (
+                  <option value={option} key={index}>{option}</option>
+                ))
+              )}
+            </select>
+          </div>
+          <div className='option' style={{ display: isVisibleDontMatch ? 'flex' : 'none' }}>
+            <h4>Don't Match :</h4>
+            <select value={dontMatchValue1} onChange={(event) => setDontMatchValue1(event.target.value)}>
+              <option value="">Choose</option>
+              {matchOptions.map((option, index) => (
+                <option value={option} key={index}>{option}</option>
+              ))}
+            </select> &
+            <select value={dontMatchValue2} onChange={(event) => setDontMatchValue2(event.target.value)}>
+              <option value="">Choose</option>
+              {typeValue === 'pairing' ? (
+                pairMatchOptions.map((option, index) => (
+                  <option value={option} key={index}>{option}</option>
+                ))
+              ) : (
+                matchOptions.map((option, index) => (
+                  <option value={option} key={index}>{option}</option>
+                ))
+              )}
+            </select>
+          </div>
         </div>
         <div className='drawButton'><button onClick={drawClick}>Draw</button></div>
+        <div className='result'>
+          <h2>Result</h2>
+          {drawResult}
+        </div>
       </div>
-      <div className='result'>
-        <h2>Result</h2>
-        {drawResult}
-      </div>
-
     </div>
   )
 }
@@ -110,3 +174,6 @@ function App() {
 
 
 export default App;
+
+
+
--- src/drawApp.js
+++ src/drawApp.js
@@ -1,36 +1,26 @@
-import React from 'react'
-
+import React from 'react';
 
 function DrawApp(props) {
     const type = props.type;
     const member = props.member.split("\n").filter(item => item !== null && item !== undefined && item !== "");;
     const groupSize = props.groupSize;
     const numberOfWinner = props.numberOfWinner;
+    const matchValue1 = props.matchValue1;
+    const matchValue2 = props.matchValue2;
+    const dontMatchValue1 = props.dontMatchValue1;
+    const dontMatchValue2 = props.dontMatchValue2;
     let winner = [];
 
-    if (type === "classic") {
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
-        shuffleArray(member);
-
-        for (let i = 0; i < numberOfWinner; i++) {
-            winner.push(member[i])
-        }
-
-
     }
-    else if (type === "christmas") {
-        const member2 = member.slice()
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
+
+    function christmas() {
+        const member2 = member.slice();
+        winner = []
 
         function compareArrays(arr1, arr2) {
             for (let i = 0; i < arr1.length; i++) {
@@ -42,65 +32,129 @@ function DrawApp(props) {
         }
 
         let sameOrder = true;
-
         while (sameOrder) {
             shuffleArray(member2);
             sameOrder = compareArrays(member, member2);
         }
-        for (let i = 0; i < member.length; i++)
+        for (let i = 0; i < member.length; i++) {
             winner.push(`${member[i]} => ${member2[i]} \n`);
+        }
 
-    }
-
-    else if (type === "grouping") {
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
+        if (matchValue1.trim() !== '' && matchValue2.trim() !== '') {
+            let found = false;
+            for (const sublist of winner) {
+                let index1 = sublist.indexOf(matchValue1);
+                let index2 = sublist.indexOf(matchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                christmas();
             }
         }
+        if (dontMatchValue1.trim() !== '' && dontMatchValue2.trim() !== '') {
+            for (const sublist of winner) {
+                let index1 = sublist.indexOf(dontMatchValue1);
+                let index2 = sublist.indexOf(dontMatchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    christmas();
+                }
+            }
+        }
+    }
+
+    function grouping() {
+        const memberTry = member.slice();
+        shuffleArray(memberTry);
+        const group = [];
+
+        while (groupSize <= memberTry.length) {
+            const groups = [];
+            for (let i = 0; i < groupSize; i++) {
+                groups.push(` ${memberTry[0]}`);
+                memberTry.shift();
+            }
+            group.push(`${groups} `);
+        }
+        winner = group;
+
+        if (matchValue1.trim() !== '' && matchValue2.trim() !== '') {
+            let found = false;
+            for (const sublist of group) {
+                let index1 = sublist.indexOf(matchValue1);
+                let index2 = sublist.indexOf(matchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                grouping();
+            }
+        }
+        if (dontMatchValue1.trim() !== '' && dontMatchValue2.trim() !== '') {
+            for (const sublist of group) {
+                let index1 = sublist.indexOf(dontMatchValue1);
+                let index2 = sublist.indexOf(dontMatchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    grouping();
+                }
+            }
+        }
+    }
+
+    function pairing() {
+        const member2 = props.member2.split("\n");
+        const pairs = [];
         shuffleArray(member);
 
-        const grup = [];
-        while (groupSize <= member.length) {
-
-            const gruplar = []
-            for (let i = 0; i < groupSize; i++) {
-                gruplar.push(` ${member[0]}`);
-                member.shift();
-
-            }
-
-            grup.push(`${gruplar} `);
-            console.log(grup)
-        }
-        winner = grup
-
-
-
-
-    }
-    else if (type === "pairing") {
-        const member2 = props.member2.split("\n");
-        const pairs = []
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
-        shuffleArray(member)
         for (let i = 0; i < member.length; i += 1) {
             const onepair = `${member[i]} - ${member2[i]} \n `;
             pairs.push(onepair);
         }
-        winner = pairs
+        winner = pairs;
+
+        if (matchValue1.trim() !== '' && matchValue2.trim() !== '') {
+            let found = false;
+            for (const sublist of pairs) {
+                let index1 = sublist.indexOf(matchValue1);
+                let index2 = sublist.indexOf(matchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                pairing();
+            }
+        }
+        if (dontMatchValue1.trim() !== '' && dontMatchValue2.trim() !== '') {
+            for (const sublist of pairs) {
+                let index1 = sublist.indexOf(dontMatchValue1);
+                let index2 = sublist.indexOf(dontMatchValue2);
+                if (index1 !== -1 && index2 !== -1) {
+                    pairing();
+                }
+            }
+        }
     }
 
-
+    if (type === "classic") {
+        shuffleArray(member);
+        for (let i = 0; i < numberOfWinner; i++) {
+            winner.push(member[i]);
+        }
+    } else if (type === "christmas") {
+        christmas();
+    } else if (type === "grouping") {
+        grouping();
+    } else if (type === "pairing") {
+        pairing();
+    }
 
     return (
-
         <div>
             {winner.map((item, index) => (
                 <span key={index}>
@@ -109,7 +163,7 @@ function DrawApp(props) {
                 </span>
             ))}
         </div>
-    )
-
+    );
 }
-export default DrawApp
+
+export default DrawApp;
